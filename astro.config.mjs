64a601c527d044da4514b19ad1530b84a2c6d0a0
Ly-sec <itslysec@gmail.com>
2025-09-16 09:35:43 +0200

    Create Matugen and Widget documentaion matugen: create a basic explanation of how to create matugen templates and hook them up widget: create a simple example widget & explanation on how to add it to the bartab etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,10 @@ export default defineConfig({
 					label: 'Getting started',
 					autogenerate: { directory: 'getting-started' },
 				},
+				{
+					label: 'Development',
+					autogenerate: { directory: 'development' },
+				},
 			],
 		}),
 	],
