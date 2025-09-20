ad806a5f0d61d55562d3296384fcb1192a3c7d29
Matan Bendix Shenhav <matan@shenhav.fyi>
2025-09-20 21:33:53 +0200

    feat(starlight): table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Noctalia Docs',
-			logo: { src: './src/assets/noctalia-logo.svg', alt: 'Noctalia' },
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/noctalia-dev' }],
-			customCss: ['./src/styles/theme.css'],
-			tableOfContents: false,
-			sidebar: [
-				{
-					label: 'Getting started',
-					autogenerate: { directory: 'getting-started' },
-				},
-				{
-					label: 'Development',
-					autogenerate: { directory: 'development' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Noctalia Docs",
+      logo: { src: "./src/assets/noctalia-logo.svg", alt: "Noctalia" },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/noctalia-dev",
+        },
+      ],
+      customCss: ["./src/styles/theme.css"],
+      tableOfContents: true,
+      sidebar: [
+        {
+          label: "Getting started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Development",
+          autogenerate: { directory: "development" },
+        },
+      ],
+    }),
+  ],
 });
