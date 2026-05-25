db7429c93a40add00b452d629d0565cbec68079e
Noctalia's server <studio@quadbyte.net>
2026-05-25 15:58:12 -0400

    better v4/v5 banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
       components: {
         Head: "./src/components/Head.astro",
         Header: "./src/components/Header.astro",
+        Banner: "./src/components/Banner.astro",
         Search: "./src/components/Search.astro",
         TableOfContents: "./src/components/TableOfContents.astro",
         Sidebar: "./src/components/Sidebar.astro",
