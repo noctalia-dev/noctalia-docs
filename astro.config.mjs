4c1e299418d82d2d7d3f50bfe022ccf67854feed
Noctalia's server <studio@quadbyte.net>
2026-06-12 10:19:45 -0400

    deeplinking to avoid og preview loosing the anchor, not perfect but better already

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,6 +43,13 @@ export default defineConfig({
             defer: true,
           },
         },
+        {
+          tag: "script",
+          attrs: {
+            src: "/section-deeplink.js",
+            defer: true,
+          },
+        },
       ],
       title: "Noctalia",
       logo: {
